beb19d9abf6feb4fa7778906114685b95fbe210b
Shure2005 <123942893+Shure2005@users.noreply.github.com>
2023-07-29 02:23:12 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("21335027", default=None, cast=int)
-API_HASH = config("76926c6bdbf143ab8fc9679be988f822", default=None)
-BOT_TOKEN = config("6092747783:AAFgtXVww1Gs30LsWNSxEZ95-KlAw-Rgfa4", default=None)
-SESSION = config("BQFFi_MAoM0vZ0d-XyRVHCLbqltZGAPUYsO7Zk6v5pgotUoU3RkIpvGnyFgIpMUcJ7AK_wluYyALLZKx8586s1Qszq5xsreXY8Squ_yYaFs0TyTP1LL9WT1ci4iRJFYbQcwtFF4Clfvfnd93NftcGPqJjABa8mRkUCq11fVToEz_j7ucBhBmKe3URAHsXTvbgCUkkUMsQPGSB0qvpuM9yAGQtlYJj7PH50SE6WHyzSXrVvkO9p_C-VKXvh2uawQRs-zmmBGkooHLrGwmHe6dhmZhjIiTRFVcKZZtgEIZ6ul2TmWadijrrRGLLIhkIZHG397Z-IKi9esCsyEPZNEkJ2aEbJCywAAAAAA6ZWADAA", default=None)
-FORCESUB = config("Cx653A", default=None)
-AUTH = config("979722243", default=None, cast=int)
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
